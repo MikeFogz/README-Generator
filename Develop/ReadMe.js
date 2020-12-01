c30950c0e59a730b7b88e96b1147aab65fc2367d
MikeFogz <MikeFogz@gmail.com>
2020-11-30 23:27:25 -0800

    UPDATE: Added inital input type prompts.

--- Develop/ReadMe.js
+++ Develop/ReadMe.js
@@ -4,11 +4,13 @@ const inquirer = require("inquirer");
 // const writeFileAsync = util.promisify(fs.writeFile);
 // const promptUser = () =>
 inquirer.prompt([
-    {
-        type: "input",
-        name: "appTitle",
-        message: "Name of your Application",
-    },
+    {type: "input", name: "appTitle", message: "Name of your Application",},
+    {type: "input", name: "appDescript", message: "Please enter a Description of your app.",},
+    {type: "input", name: "appInstall", message: "Please provide Installation Instructions.",},
+    {type: "input", name: "appUsage", message: "Please provide Usage Information.",},
+    {type: "input", name: "appContrib", message: "What are your Guidelines for Contributors?",},
+    {type: "input", name: "appTest", message: "How can one start and test your app?",},
+
 ])
 .then((res) => {
     console.log(res)
