77d59a65b328fbfa933b80698d91195f293b8528
MikeFogz <MikeFogz@gmail.com>
2020-12-04 02:13:17 -0800

    UPDATE: Updated project's readme, added screenshot of code, made sample README.md using generator (under Develop folder)

---
+++ Develop/README.md
@@ -0,0 +1,51 @@
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+
+# ReadMe Generator
+
+## Description
+
+This is sample description.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Sample Instructions
+
+## Usage
+
+Here are instructions on how to use this app.
+
+Here is where a screenshot would be:
+
+![Screenshot of App](./assets/screenshot.png) Include screenshots as needed.
+
+## Contributing
+
+None
+
+## Credits
+
+List of contirbutors I worked with and tools/tutorials used on this project:
+
+## License
+
+MIT
+
+## Tests
+
+N/A
+
+## Questions
+
+You can find me on GitHub [@MikeFogz](https://github.com/MikeFogz)
+
+I can be contacted at MikeFogz@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,15 +1,68 @@
 // function to generate markdown for README
-// function licenseSection(license){
-//   if (license !== "None"){
-//     return(`This License is ${answers.appLicense}`)
-//   }
-// }
+function licenseSection(license){
+  if (license !== "None"){
+    return(`This License is ${answers.appLicense}`)
+  }
+}
 
 
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+  return `![badge](https://img.shields.io/badge/license-${data.appLicense}-brightgreen)
+  # ${data.appTitle}
 
-`;
+  ## Description 
+
+  ${data.appDescription}
+
+
+  ## Table of Contents
+
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+
+  ## Installation
+
+  ${data.appInstall}
+
+  ## Usage 
+  Here are instructions on how to use this app. 
+  ${data.appUsage}
+  ![Screenshot of App](${data.appPic1}) Include screenshots as needed. 
+
+  ## Contributing
+
+  ${data.appContrib}
+
+
+  ## Credits
+
+  List of contirbutors I worked with and tools/tutorials used on this project: 
+
+  ${data.appCredits}
+
+  ## License
+
+  ${data.appLicense}
+
+
+  ## Tests
+
+  ${data.appTest}
+
+  ## Questions
+
+  You can find me on GitHub [@${data.appGitHub}](https://github.com/${data.appGitHub})
+
+  I can be contacted at ${data.appEmail}
+  `;
 }
 
 module.exports = generateMarkdown;
